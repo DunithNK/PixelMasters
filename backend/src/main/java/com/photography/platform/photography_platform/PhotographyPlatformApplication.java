5a1d384887696c2ee45923c444dee49b608ec50c
Dunith <Dunithnk@gmail.com>
2025-03-23 16:04:18 +0530

    Add backend folder

---
+++ backend/src/main/java/com/photography/platform/photography_platform/PhotographyPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.photography.platform.photography_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotographyPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotographyPlatformApplication.class, args);
+	}
+
+}
